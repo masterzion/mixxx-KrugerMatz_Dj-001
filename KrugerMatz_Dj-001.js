66ae15543579d94c534fb65e92d6b1eca2477d39
Jairo Moreno <master.zion@gmail.com>
2018-01-29 21:08:36 +0100

    working version

--- KrugerMatz_Dj-001.js
+++ KrugerMatz_Dj-001.js
@@ -2,64 +2,45 @@ function KrugerMatz001(){}
 
 KrugerMatz001.wheelTouch = function (channel, control, value, status, group) {
   if ('[Channel2]' == group) {
-      currentDeck = 2;
+    currentDeck = 2;
   } else {
-      currentDeck = 1;
+    currentDeck = 1;
   }
-    if ((status & 0xF0) === 0x90) {    // If button down
-//  if (value === 0x7F) {  // Some wheels send 0x90 on press and release, so you need to check the value
-        var alpha = 1.0/8;
-        var beta = alpha/32;
-        engine.scratchEnable(currentDeck, 128, 15+1/3, alpha, beta);
-    } else {    // If button up
-        engine.scratchDisable(currentDeck);
-    }
+
+  if ((status & 0xF0) === 0x90) {    // If button down
+      var alpha = 1.0/8;
+      var beta = alpha/32;
+      engine.scratchEnable(currentDeck, 128, 15+1/3, alpha, beta);
+  } else {    // If button up
+      engine.scratchDisable(currentDeck);
+  }
 }
 
 // The wheel that actually controls the scratching
 KrugerMatz001.wheelTurn = function (channel, control, value, status, group) {
   if ('[Channel2]' == group) {
-      currentDeck = 2;
+    currentDeck = 2;
   } else {
-      currentDeck = 1;
+    currentDeck = 1;
   }
 
-  print(group);
+  var newValue = value - 32;
 
-    // --- Choose only one of the following!
-
-    // A: For a control that centers on 0:
-    var newValue;
-    if (value < 64) {
-        newValue = value;
-    } else {
-        newValue = value - 128;
-    }
-
-    // B: For a control that centers on 0x40 (64):
-    var newValue = value - 64;
-
-
-    if (currentDeck == 1) {
-        newValue = newValue * -1;
-    }
-
-    // --- End choice
-
-    // In either case, register the movement
-    if (engine.isScratching(currentDeck)) {
-        engine.scratchTick(currentDeck, newValue); // Scratch!
-    } else {
-        engine.setValue('[Channel'+currentDeck+']', 'jog', newValue); // Pitch bend
-    }
+  if (currentDeck == 1) {
+      newValue = newValue * -1;
+  }
+  print(newValue)
+  if (engine.isScratching(currentDeck)) {
+      engine.scratchTick(currentDeck, newValue); // Scratch!
+  } else {
+      engine.setValue('[Channel'+currentDeck+']', 'jog', newValue); // Pitch bend
+  }
 }
 
-KrugerMatz001.cursorTurn = function (channel, control, value, status, group) {
-    if (value === 0x7F) {    // If button down
-        print("down")
-        engine.MoveDown;
-    } else {    // If button up
-        print("up")
-        engine.MoveUp;
+KrugerMatz001.rotarySelector = function(channel, control, value, status) {
+    if (value === 0x7F) {
+        engine.setValue("[Library]", "MoveDown", 1);
+    } else {
+        engine.setValue("[Library]", "MoveUp", 1);
     }
 }
